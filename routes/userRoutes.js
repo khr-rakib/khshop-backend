c260d4a27b16a89c2f9ee807d8b08b8ecd61854e
khr-rakib <khr.rakib@gmail.com>
2021-05-17 01:07:26 +0600

    admin stuff - 1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,26 @@
 import express from 'express';
 const router = express.Router();
-import { protect } from '../middlewares/authMiddleware.js';
+import { protect, admin } from '../middlewares/authMiddleware.js';
 import {
   authUser,
   authUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js';
 
 router.post('/', registerUser);
+router.get('/', protect, admin, getUsers);
 router.post('/login', authUser);
 router
   .route('/profile')
   .get(protect, authUserProfile)
   .put(protect, updateUserProfile);
+router.delete('/:id', protect, admin, deleteUser);
+router.get('/:id', protect, admin, getUserById);
+router.put('/:id', protect, admin, updateUser);
 
 export default router;
